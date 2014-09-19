3aee6ffc7de920b19371440ae0ee4e6464384764
Ashley Daniell <adaniell@hbs.edu>
2014-09-19 13:47:39 -0400

    Added images and updated index and css file

--- index.php
+++ index.php
@@ -5,36 +5,59 @@
 <link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Oswald:400,700|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
 
+<div id="container">
+
 <div id="div1">
 <h1>Ashley Daniell</h1>
+<img class="img-border" src="/p1/images/me.png" width="210" height="300" />
+
+
+
+<ul>
 <h2>Dynamic Web<br/> Applications Portfolio</h2>
-<img class="img-border" src="/images/me.png" width="210" height="300" />
-
-
+<li>
 <h2>Project 2</h2>
-<p><a href="http://p2.ashleydaniell.com">View</a>
+<p><a href="http://p2.ashleydaniell.com">View</a>&nbsp;&nbsp;
 <a href="https://github.com/adaniell/p2">Github</a></p>
+</li>
 
+<li>
 <h2>Project 3</h2>
-<p><a href="http://p3.ashleydaniell.com">View</a>
+<p><a href="http://p3.ashleydaniell.com">View</a>&nbsp;&nbsp;
 <a href="https://github.com/adaniell/p3">Github</a></p>
+</li>
 
+<li>
 <h2>Project 4</h2>
-<p><a href="http://p4.ashleydaniell.com">View</a>
+<p><a href="http://p4.ashleydaniell.com">View</a>&nbsp;&nbsp;
 <a href="https://github.com/adaniell/p4">Github</a></p>
+</li>
+</ul>
+
+
+
 </div>
 
 <div id="div2">
 
 <h3>In 2+ sentences describe who you are and what you do.</h3>
-<p>My name is Ashley Daniell and I am from Medford, MA. I graduted in 2012 from the University of Massachusetts Dartmouth with a BA in Crime &amp; Justice studies. I've always been interested in computer technology, and I sometimes regret that I didn't major in it. But now I have a great job as a web developer at Harvard Business School, where I first started just copying and pasting content 4 years ago. </p>
+<p>My name is Ashley Daniell and I am from Medford, MA. I graduted in 2012 from the University of Massachusetts Dartmouth with a BA in Crime &amp; Justice Studies. I've always been interested in computers and technology, and I sometimes regret that I didn't major in something like Computer Science. But now I have a wonderful job as a web developer/technologist at Harvard Business School. I first started just copying and pasting content 4 years ago on an Intranet migration project, which then triggered my interest in web development. </p>
+
 <h3>Why are you taking this course?</h3>
-<p>I am taking this course to further expand my web development skills, as well to complete the requirements for the Web Technologies Certificate.</p>
+<p>I am taking this course to further expand my web development skills, learn how dynamic applications work, and to be able to understand and build a page with personilzation. I am also working towards completing the requirements for the Professional Graduate Web Technologies Certificate.</p>
+
 <h3>What previous experience do you have with programming?</h3>
-<p>Not that much, I would consider myself a beginner. I know the basics of Javascript but have been mostly working with html/css and xslt. 
+<p>Not that much, I would consider myself a beginner. I know the fundamentals of Javascript and some XSLT but have been mostly working with HTML &amp CSS. </p>
+
 <h3>Are you working on a Mac, PC, or some other platform?</h3>
-<p>I am working on a PC with Windows 7. I use Mac's at work sometimes but I prefer the PC.</p>
+<p>I am working on a PC with Windows 7. I use a Mac at work for testing purposes and I found that I prefer the PC.</p>
+
 <h3>Anything else we should know?</h3>
+
+<p>I am at the point in my life where I really want to buckle down and focus on Web Development as my career. This class will take me one step closer and I'm very excited about it. Although I love my position at HBS, I am currently contracting under my own LLC, and I'm hoping the Certificate will help me get a full time position.</p>
+<p>I recently received an "In Recognition" Award recommended by my Director at work, for the last project I was on. It was a re-design of Professor Michael Porter's HBS website, which was an ugly static site for many many years. I was dedicated to this project for about 8 months, so it felt amazing to be recognized for all of the hard work I put into it. <br/>Check it out! <a href="http://www.isc.hbs.edu" target="_blank"> Institute for Strategy &amp; Competitiveness</a>   </p>
+</div>
+
 </div>
 </body>
 </html>
