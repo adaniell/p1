fabfa631d6c8e2fa5a452516ce2f5ed762bbfaae
Ashley Daniell <adaniell@hbs.edu>
2014-09-18 22:00:09 -0400

    Updated index file and added stylesheet

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+<link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,700|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+
+<div id="div1">
 <h1>Ashley Daniell</h1>
-<h2>Project 1</h2>
+<h2>Dynamic Web<br/> Applications Portfolio</h2>
+<img class="img-border" src="/images/me.png" width="210" height="300" />
+
+
+<h2>Project 2</h2>
+<p><a href="http://p2.ashleydaniell.com">View</a>
+<a href="https://github.com/adaniell/p2">Github</a></p>
+
+<h2>Project 3</h2>
+<p><a href="http://p3.ashleydaniell.com">View</a>
+<a href="https://github.com/adaniell/p3">Github</a></p>
+
+<h2>Project 4</h2>
+<p><a href="http://p4.ashleydaniell.com">View</a>
+<a href="https://github.com/adaniell/p4">Github</a></p>
+</div>
+
+<div id="div2">
+
+<h3>In 2+ sentences describe who you are and what you do.</h3>
+<p>My name is Ashley Daniell and I am from Medford, MA. I graduted in 2012 from the University of Massachusetts Dartmouth with a BA in Crime &amp; Justice studies. I've always been interested in computer technology, and I sometimes regret that I didn't major in it. But now I have a great job as a web developer at Harvard Business School, where I first started just copying and pasting content 4 years ago. </p>
+<h3>Why are you taking this course?</h3>
+<p>I am taking this course to further expand my web development skills, as well to complete the requirements for the Web Technologies Certificate.</p>
+<h3>What previous experience do you have with programming?</h3>
+<p>Not that much, I would consider myself a beginner. I know the basics of Javascript but have been mostly working with html/css and xslt. 
+<h3>Are you working on a Mac, PC, or some other platform?</h3>
+<p>I am working on a PC with Windows 7. I use Mac's at work sometimes but I prefer the PC.</p>
+<h3>Anything else we should know?</h3>
+</div>
+</body>
+</html>
